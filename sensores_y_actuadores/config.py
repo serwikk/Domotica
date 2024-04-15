c05120c73587933ea7209d95d17433c6fe15d2a2
Serwikk <012sergio210@gmail.com>
2024-04-15 22:48:28 +0200

    NEW: config.py creado para facilitar la importación de valores de configuración

---
+++ sensores_y_actuadores/config.py
@@ -0,0 +1,19 @@
+import toml
+
+def obtener_valores_config(sensor):
+
+    ruta_archivo_conf = '/home/serwikk/Domotica/sensores_y_actuadores'
+    try:
+        with open(f'{ruta_archivo_conf}/conf.toml', 'r') as file:
+            configuracion = toml.load(file)
+
+        valores_sensor = configuracion[sensor]
+
+        return valores_sensor.values()
+
+    except Exception as e:
+
+        print(f"Ha ocurrido un error de tipo: {e}") # En un futuro, cambiarlo por un registro de logs
+        raise e
+
+(TEMPERATURA_MIN, TEMPERATURA_MAX) = obtener_valores_config('sensor_temperatura')

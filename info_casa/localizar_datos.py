f5fbf6348bf692d15858ab19d6f574e9e580f5c7
Serwikk <012sergio210@gmail.com>
2024-04-20 00:36:52 +0200

    Ejemplo de casa creada

---
+++ info_casa/localizar_datos.py
@@ -0,0 +1,18 @@
+import json
+
+
+def obtener_datos():
+
+    with open("casa_ejemplo.json", "r") as file:
+
+        return json.load(file)
+
+
+
+
+if __name__=="__main__":
+
+    datos = obtener_datos()
+
+
+    

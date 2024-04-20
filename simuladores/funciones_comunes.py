03e35541400a91761593ab7deca145fa02d5e9b4
Serwikk <012sergio210@gmail.com>
2024-04-20 23:11:26 +0200

    UPDATE: Comentarios en funciones comunes

--- simuladores/funciones_comunes.py
+++ simuladores/funciones_comunes.py
@@ -5,11 +5,17 @@ import math
 import string
 import json
 
+########################################################################################################
+# FUNCIONES PARA CAMBIO DE FORMATOS
 
 def hora_datetime_a_float(hora: float) -> float:
     hora_float = hora.hour + hora.minute / 60
 
     return round(hora_float, 2)
+
+
+########################################################################################################
+# FUNCIONES PARA GENERACIÓN DE VALORES
 
 def generar_valor_distribucion_normal(valor_minimo: float, valor_maximo: float) -> float:
 
@@ -25,17 +31,6 @@ def generar_valor_distribucion_normal(valor_minimo: float, valor_maximo: float) 
 def generar_outlier(min_outlier: float, max_outlier: float) -> float:
 
     return rd.uniform(min_outlier, max_outlier)
-
-def generar_valor_ciclico(hora: int, valor_min: float, valor_max: float) -> float:
-
-    amplitud = (valor_max - valor_min) / 2
-    valor_medio = (valor_max + valor_min) / 2
-
-    angulo = (hora / 24) * 2 * math.pi
-
-    valor = valor_medio + amplitud * math.sin(angulo)
-
-    return round(valor, 2)
 
 
 def generar_valor_ciclico(hora: float, valor_min: float, valor_max: float, umbral: float = 0.7, fase: float = math.radians(170)) -> float:
@@ -53,6 +48,8 @@ def generar_valor_ciclico(hora: float, valor_min: float, valor_max: float, umbra
     
     return valor_final
     
+###########################################################################################
+# FUNCIÓN PARA LA GENERACIÓN DE IDs
 
 def generar_id_aleatorio(prefijo = None, longitud=10):
     caracteres = string.ascii_letters + string.digits
@@ -63,6 +60,8 @@ def generar_id_aleatorio(prefijo = None, longitud=10):
     
     return id_aleatorio
 
+############################################################################################
+# FUNCIONES PARA LA LECTURA Y ESCRITURA DE ARCHIVOS
 
 def registrar_en_csv(ruta, campos, dato_nuevo):
 
@@ -79,6 +78,8 @@ def registrar_en_csv(ruta, campos, dato_nuevo):
         csvwriter.writerow(dato_nuevo)
 
     print("Registro guardado correctamente en", ruta)
+
+
 
 def leer_valor_magnitud(magnitud, direccion_archivo):
     

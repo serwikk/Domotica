4bfba468b87e8313a2fdd93711d1c78e93781b91
Serwikk <012sergio210@gmail.com>
2024-04-20 20:26:42 +0200

    estados_del_arte.md movido a otra subcarpeta que no se cacheará

---
+++ controlador/controlador.py
@@ -0,0 +1,21 @@
+from sensores_y_actuadores import sensor_temperatura
+
+class Controlador():
+
+    def __init__(self, espacio):
+        self.espacio = espacio
+
+
+
+
+
+def main():
+    controlador = Controlador(espacio = "Habitación 1")
+
+    sensor_temperatura1 = sensor_temperatura.SensorTemperatura(id = '2324', tipo = 'sensor_temperatura1')
+
+    print(vars(sensor_temperatura1))
+
+
+if __name__=="__main__":
+    main()

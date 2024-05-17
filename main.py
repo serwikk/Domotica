4fc7c1e6abbdb16a7e7ff957914c604ddfbc9cef
serwikk <012sergio210@gmail.com>
2024-05-17 18:05:35 +0200

    Minor changes

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from sensores_y_actuadores import sensor_temperatura as st, sensor_humedad as sh
 def main():
 
     controlador = Controlador("Habitación 1", sensores=[st.SensorTemperatura(), sh.SensorHumedad(), sl.SensorLuz()])
+    # controlador.inicializar_valores()
+    # print(st.SensorTemperatura().obtener_valor())
 
 
 if __name__=="__main__":

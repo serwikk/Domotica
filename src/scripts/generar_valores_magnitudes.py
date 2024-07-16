41bb9f88fa91d75934e18405de64ead7820db528
serwikk <012sergio210@gmail.com>
2024-07-17 00:27:19 +0200

    generar_valores_aleatorios creándose

--- src/scripts/generar_valores_magnitudes.py
+++ src/scripts/generar_valores_magnitudes.py
@@ -1,13 +1,22 @@
 from src.handlers.csv_handler import CSVHandler
+from src.handlers.datetime_handler import DatetimeHandler
+from src.handlers import generation_handler
 
 
 def main():
-    
+
+    datetime_handler = DatetimeHandler()
     csv_handler = CSVHandler('/home/serwikk/Domotica/src/handlers/csv/temperaturas_hora_mes_vitoria.csv')
 
-    print(csv_handler.valores)
+    valor_temperatura = csv_handler.buscar_valor_celda(datetime_handler.hora, datetime_handler.obtener_mes_string())
 
-    pass
+    valor_final = generation_handler.agregar_umbral_a_valor(valor_temperatura)
+
+    print(valor_final)
+
+    
+
+
 
 if __name__=="__main__":
     main()
